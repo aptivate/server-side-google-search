d5e1bde49ff4ad96468ba1065171685ef7f895eb
Martin Burchell <martinb@aptivate.org>
2014-12-18 16:59:50 +0000

    Made ssgs-google-search.php code standards compliant

--- ssgs-google-search.php
+++ ssgs-google-search.php
@@ -1,64 +1,63 @@
 <?php
 /*
-Plugin Name: Server-Side Google Search
-Description: Google Custom Search for your site, without JavaScript
-Version: 1.0.0
-Author: Aptivate, WebshopLogic
-License: GPLv2 or later
-Text Domain: ssgs
-Requires at least: 3.7
-Tested up to: 3.9
+  Plugin Name: Server-Side Google Search
+  Description: Google Custom Search for your site, without JavaScript
+  Version: 1.0.0
+  Author: Aptivate
+  License: GPLv2 or later
+  Text Domain: ssgs
+  Requires at least: 3.7
+  Tested up to: 4.0
 */
 
 if ( ! class_exists( 'SS_Google_Search' ) ) {
 
-class SS_Google_Search {
+	class SS_Google_Search {
 
-	public $plugin_path;
+		public $plugin_path;
+		public $plugin_url;
 
-	public $plugin_url;
+		function __construct() {
 
+			include_once( 'ssgs-admin-page.php' );
 
-	function __construct() {
+			add_action( 'init', array( $this, 'init' ), 0 );
 
-		include_once( 'ssgs-admin-page.php' );
+			include_once( 'ssgs-widget.php' );
+			do_action( 'ssgs_init' );
+		}
 
-		add_action( 'init', array( $this, 'init' ), 0 );
+		public function init() {
+			load_plugin_textdomain( 'ssgs', false,
+									dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-		include_once( 'ssgs-widget.php' );
-		do_action( 'ssgs_init' );
+			global $ssgs_admin_page;
+			$ssgs_admin_page = new SSGS_Admin_Page;
+
+			$deps = array();
+			$css_version = 3;
+			wp_enqueue_style( 'ssgs', plugins_url( 'ssgs.css', __FILE__ ),
+								$deps, $css_version );
+
+			do_action( 'ssgs_init', $this );
+		}
+
+		public function plugin_path() {
+			if ( $this->plugin_path ) {
+				return $this->plugin_path;
+			}
+
+			return $this->plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) );
+		}
+
+		function plugin_url() {
+			if ( $this->plugin_url ) {
+				return $this->plugin_url;
+			}
+			return $this->plugin_url = untrailingslashit( plugins_url( '/', __FILE__ ) );
+		}
 	}
 
-	public function init() {
-
-		load_plugin_textdomain( 'ssgs', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-
-		global $ssgs_admin_page;
-		$ssgs_admin_page = new SSGS_Admin_Page;
-
-		$deps = array();
-		$css_version = 3;
-		wp_enqueue_style( 'ssgs', plugins_url('ssgs.css', __FILE__), $deps, $css_version );
-
-		do_action( 'ssgs_init', $this );
-	}
-
-	public function plugin_path() {
-		if ( $this->plugin_path ) return $this->plugin_path;
-
-		return $this->plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) );
-	}
-
-	function plugin_url() {
-		if ( $this->plugin_url ) return $this->plugin_url;
-		return $this->plugin_url = untrailingslashit( plugins_url( '/', __FILE__ ) );
-	}
-
-
+	$GLOBALS['ss_google_search'] = new SS_Google_Search();
 }
-
-$GLOBALS['ss_google_search'] = new SS_Google_Search();
-
-}
-
 ?>
